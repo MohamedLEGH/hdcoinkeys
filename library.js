749cbd8b5872e6e078b13d742afcf80f7685bdb0
MohamedLEGH <mohamed.amine.legheraba@gmail.com>
2018-09-16 13:32:13 +0200

    factorization of code

--- library.js
+++ library.js
@@ -7,6 +7,8 @@ var CoinKey = require('coinkey')
 var ethUtil = require('ethereumjs-util')
 // new Buffer(seed, 'hex')
 
+// Following https://iancoleman.io/bip39/
+
 function create_words(){
     return bip39.generateMnemonic(strength=256);
 }
@@ -27,6 +29,8 @@ function rootkey_from_seed(seed,network){
     }
     else if(network === 'bitcoin-testnet'){
         var testnet = coininfo.bitcoin.test.versions.bip32;
+        //coininfo('BTC-TEST')
+        //coininfo('BTC-TEST')['versions']['bip32']
         var hdkey = HDKey.fromMasterSeed(seed,testnet);
     }
     else {
@@ -34,6 +38,26 @@ function rootkey_from_seed(seed,network){
         throw "Wrong Network : should be ethereum or bitcoin or bitcoin-testnet!";
     }
     return hdkey
+}
+
+function ethereum_account_to_addr(p){
+    const pubKey = ethUtil.privateToPublic(p);
+    const addr = ethUtil.publicToAddress(pubKey).toString('hex');
+    const address = ethUtil.toChecksumAddress(addr);
+    return address
+}
+
+function account_from_privatekey(key,network){
+    if(network === 'bitcoin'){
+        return CoinKey(key)
+    }
+    else if(network === 'bitcoin-testnet'){
+        return new CoinKey(key,coininfo('BTC-TEST'))
+    }
+    else if(network === 'ethereum'){
+        return [key,ethereum_account_to_addr(key)]
+    }
+
 }
 
 function account_from_rootkey(key,account_number,network){
@@ -55,16 +79,17 @@ function account_from_rootkey(key,account_number,network){
     
     path = `m/44'/${net}'/0'/0/${index}`
     account = key.derive(path)
-    return account.privateKey
+    return account_from_privatekey(account.privateKey,network)
 }
 
-function ethereum_priv_to_addr(p){
-    const pubKey = ethUtil.privateToPublic(p);
-    const addr = ethUtil.publicToAddress(pubKey).toString('hex');
-    const address = ethUtil.toChecksumAddress(addr);
-    return address
+/*
+function bitcoin_fromkey(p){
+    return new CoinKey(pr)
 }
-
+function bitcointestnet_fromkey(p){
+    return new CoinKey(pr,coininfo('BTC-TEST'))
+}
+*/
 /////////////// Test ////////////////
 
 var test = create_words();
@@ -96,13 +121,26 @@ console.log(hdkey.publicExtendedKey)
 
 */
 var t = rootkey_from_seed(testS,network='bitcoin')
-var pr = account_from_rootkey(t,account_number=0,network='bitcoin')
-//var addr = ethereum_priv_to_addr(pr)
-var key = new CoinKey(new Buffer.from(pr, 'hex'))
+var bitcoin = account_from_rootkey(t,account_number=0,network='bitcoin')
+var bitcointest = account_from_rootkey(t,account_number=0,network='bitcoin-testnet')
+var ethereum = account_from_rootkey(t,account_number=0,network='ethereum')
 
+//var addr = ethereum_priv_to_addr(pr)
+//var bitcoin = bitcoin_fromkey(pr)
+//var bitcointest = bitcointestnet_fromkey(prt)
 console.log("*********Zero*********")
 
-console.log(key.privateWif)
+console.log(bitcoin.privateWif)
 console.log("*********Zero Address*********")
-//console.log(addr)
-console.log(key.publicAddress) 
+console.log(bitcoin.publicAddress)
+
+console.log("*********Zero Testnet*********")
+console.log(bitcointest.privateWif)
+console.log("*********Zero Address*********")
+console.log(bitcointest.publicAddress)
+
+console.log("*********Zero Ethereum*********")
+console.log(ethereum[0].toString('hex'))
+console.log("*********Zero Address*********")
+console.log(ethereum[1])
+
